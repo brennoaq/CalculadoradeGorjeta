dcaf07de4b90a035864bd1a41089d602517d7552
brenno araujo queiroz <brennoaraujoqueiroz@gmail.com>
2019-08-27 00:12:45 -0300

    fist commit project complet

---
+++ app/src/main/java/com/example/calculadoradegorjeta/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.calculadoradegorjeta;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText editValor;
+    private TextView textPorcentagem;
+    private TextView textGorjeta;
+    private TextView textTotal;
+    private SeekBar seekGorjeta;
+
+    private double porcentagem = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editValor = findViewById(R.id.editValor);
+        textPorcentagem = findViewById(R.id.textPorcentagem);
+        textGorjeta = findViewById(R.id.textGorjeta);
+        textTotal = findViewById(R.id.textTotal);
+        seekGorjeta = findViewById(R.id.seekGorjeta);
+
+
+        //Controlar seekbar
+        seekGorjeta.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                porcentagem = seekBar.getProgress();
+                textPorcentagem.setText(Math.round(porcentagem) + "%");
+                calcular();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+    private void calcular() {
+
+        double valorDigitado = Double.parseDouble(editValor.getText().toString());
+
+        double gorjeta = valorDigitado * (porcentagem/100);
+
+        double total = gorjeta + valorDigitado;
+
+        textGorjeta.setText("R$" +  Math.round(gorjeta));
+        textTotal.setText("R$" + total);
+
+    }
+}
